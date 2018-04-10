1341953b62232c274fc6881434946e480dc0fd69
Alan Scarpa <alan.scarpa@gmail.com>
2018-04-09 18:51:48 -0700

    button tap going to next vc

---
+++ ArtHouse/View Controllers/Onboarding/EnableCameraViewController.swift
@@ -0,0 +1,40 @@
+//
+//  EnableCameraViewController.swift
+//  ArtHouse
+//
+//  Created by Alan Scarpa on 4/7/18.
+//  Copyright © 2018 The Scarpa Group. All rights reserved.
+//
+
+import UIKit
+
+class EnableCameraViewController: UIViewController {
+
+    weak var delegate: OnboardingDelegate?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func nextButtonTapped() {
+        delegate?.nextButtonTapped()
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
