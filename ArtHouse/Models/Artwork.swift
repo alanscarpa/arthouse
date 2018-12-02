2eae77c0a2944314d5d67ef55170d343e76f03d9
Alan Scarpa <alan.scarpa@gmail.com>
2018-12-02 12:50:46 -0800

    artwork model started

---
+++ ArtHouse/Models/Artwork.swift
@@ -0,0 +1,17 @@
+//
+//  Artwork.swift
+//  ArtHouse
+//
+//  Created by Alan Scarpa on 12/2/18.
+//  Copyright © 2018 The Scarpa Group. All rights reserved.
+//
+
+import UIKit
+
+struct Artwork {
+    let title: String
+    let height: Int
+    let width: Int
+    let depth: Int
+    let image: UIImage
+}
