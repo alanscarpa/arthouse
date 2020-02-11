a036ae792db0f7611da445245cdafb24ed1c7cc5
Alan Scarpa <ascarpa@apple.com>
2020-02-11 08:36:07 -0500

    Fix crash when paging back onboarding

--- ArtHouse/View Controllers/Onboarding/EnableCameraViewController.swift
+++ ArtHouse/View Controllers/Onboarding/EnableCameraViewController.swift
@@ -18,8 +18,8 @@ class EnableCameraViewController: OnboardingChildViewController {
         enableCameraButton.roundCorners()
     }
 
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         if AVCaptureDevice.authorizationStatus(for: .video) == .authorized {
             self.delegate?.nextButtonTapped()
         }
